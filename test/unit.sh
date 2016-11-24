87f6af0d7a5cef1fc4f9806d81f3367436acbec3
Dane Springmeyer <dane@mapbox.com>
2016-11-24 11:41:07 -0800

    adapt test assertion to work with both gdb and lldb

--- test/unit.sh
+++ test/unit.sh
@@ -46,7 +46,7 @@ assertEqual "$?" "0" "able to compile program abort.cpp"
 assertEqual "${RESULT}" "134" "emitted expected signal"
 assertEqual "$(head -n 1 logs.txt)" "${WORKING_DIR}/run-test exited with code:134" "Emitted expected first line of stdout"
 assertContains "$(head -n 2 logs.txt | tail -n 1)" "Found core at" "Found core file for given PID"
-assertContains "$(cat logs.txt)" "at abort.cpp:2" "Found expected line number in backtrace output"
+assertContains "$(cat logs.txt)" "abort.cpp:2" "Found expected line number in backtrace output"
 #cat logs.txt
 
 
@@ -62,7 +62,7 @@ assertEqual "$?" "0" "able to compile program segfault.cpp"
 assertEqual "${RESULT}" "139" "emitted expected signal from segfault"
 assertEqual "$(head -n 1 logs.txt)" "${WORKING_DIR}/run-test exited with code:139" "Emitted expected first line of stdout"
 assertContains "$(head -n 2 logs.txt | tail -n 1)" "Found core at" "Found core file for given PID"
-assertContains "$(cat logs.txt)" "at segfault.cpp:2" "Found expected line number in backtrace output"
+assertContains "$(cat logs.txt)" "segfault.cpp:2" "Found expected line number in backtrace output"
 #cat logs.txt
 
 # bus error
@@ -77,7 +77,7 @@ assertEqual "$?" "0" "able to compile program bus_error.cpp"
 assertEqual "${RESULT}" "138" "emitted expected signal from bus error"
 assertEqual "$(head -n 1 logs.txt)" "${WORKING_DIR}/run-test exited with code:138" "Emitted expected first line of stdout"
 assertContains "$(head -n 2 logs.txt | tail -n 1)" "Found core at" "Found core file for given PID"
-assertContains "$(cat logs.txt)" "at bus_error.cpp:2" "Found expected line number in backtrace output"
+assertContains "$(cat logs.txt)" "bus_error.cpp:2" "Found expected line number in backtrace output"
 #cat logs.txt
 
 if [[ ${CODE} == 0 ]]; then
