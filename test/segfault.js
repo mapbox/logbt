e7b952e213adcaf974b27220b0fb93202538ef2f
Dane Springmeyer <dane@mapbox.com>
2016-12-01 11:29:06 -0800

    support node v0.10.x

--- test/segfault.js
+++ test/segfault.js
@@ -2,7 +2,7 @@ process.title = 'custom-node'
 
 console.log('running',process.title);
 
-setTimeout(() => {
+setTimeout(function() {
   console.error("going to crash",process.title);
   process.kill(process.pid, 'SIGSEGV');
 }, 1000);
